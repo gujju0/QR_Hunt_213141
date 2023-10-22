8043f16951c54d21171ea355f77e8829a24ad8ce
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:29:50 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following is not a type of computer on the basis of operation?",
+    answer: "Generator",
+    options: [
+      "AV101-A",
+      "AV007-D",
+      "AV108-A",
+      "AV109-C"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Allows the user to create and maintain a personal list of favorite URL addresses.",
+    answer: "Carry",
+    options: [
+      "AV112-C",
+      "AV109-D",
+      "AV108-A",
+      "AV012-D"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The most common format for a home video recorder is VHS. VHS stands for...?",
+    answer: "Python",
+    options: [
+      "AV112-A",
+      "AV005-D",
+      "AV010-C",
+      "AV104-A"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a type of computer code?",
+    answer: "Parrot",
+    options: [
+      "AV112-D",
+      "AV114-B",
+      "AV007-D",
+      "AV002-B"
+    ]
+  },
+  {
+    numb: 5,
+    question: "A program that can execute high-level language programs.",
+    answer: "Drive",
+    options: [
+      "AV006-C",
+      "AV103-A",
+      "AV004-B",
+      "AV011-A"
+    ]
+  },
+  {
+    numb: 6,
+    question: "In which decade was the SPICE simulator introduced?",
+    answer: "Macbook",
+    options: [
+      "AV014-C",
+      "AV101-B",
+      "AV003-C",
+      "AV111-A"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following devices provides the communication between a computer and the outer world?",
+    answer: "Pizza",
+    options: [
+      "AV014-D",
+      "AV004-A",
+      "AV112-C",
+      "AV005-A"
+    ]
+  },
+  
+];
